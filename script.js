d304c38adc09395828d2626f8847b19ba5e43da4
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-05 22:36:39 -0500

    password generate in progress

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ let useSymbols = window.confirm("Use symbols?");
 let generateBtn = document.getElementById("generate");
 let resultBtn = document.getElementById("result");
 let symbols = '~`!@#$%^&*()_-+={[}]:;"<,>.?/|';
+
+generateBtn.addEventListener("click", function () {
+    let Length = totalLength;
+    let Lowercase = useLowercase;
+    let Uppercase = useUppercase;
+    let Numbers = useNumbers;
+    let Symbols = useSymbols;
+    resultBtn.innerText = passwordGenerate(Lowercase, Uppercase, Numbers, Symbols, Length);
+  });
+  
