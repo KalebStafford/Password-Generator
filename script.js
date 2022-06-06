33033ad14a0f3f998042c8b23d4388224961b1db
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-06 00:07:42 -0500

    password in progress

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let totalLength = window.prompt("Password Length:");
-let useNumbers = window.confirm("Use numbers?");
-let useUppercase = window.confirm("Use uppercase letters?");
-let useLowercase = window.confirm("Use lowercase letters?");
-let useSymbols = window.confirm("Use symbols?");
-let generateBtn = document.getElementById("generate");
-let resultBtn = document.getElementById("result");
 let symbols = '~`!@#$%^&*()_-+={[}]:;"<,>.?/|';
 
 function randomLowercase() {
@@ -30,12 +23,27 @@ let randomGeneration = {
   symbols: randomSymbols,
 };
 
+function passwordGenerate(lowercase, uppercase, numbers, symbols, length) {
+  let generatedPassword = "";
+  let TotalGenerated = lowercase + uppercase + numbers + symbols;
+  let TotalArranged = [{ lowercase }, { uppercase }, { numbers }, { symbols }]
+  .filter((data) => Object.values(data)[0]);
+// In progress
+}
+
+let totalLength = window.prompt("Password Length:");
+let useNumbers = window.confirm("Use numbers?");
+let useUppercase = window.confirm("Use uppercase letters?");
+let useLowercase = window.confirm("Use lowercase letters?");
+let useSymbols = window.confirm("Use symbols?");
+let generateBtn = document.getElementById("generate");
+let resultBtn = document.getElementById("result");
+
 generateBtn.addEventListener("click", function () {
-    let Length = totalLength;
-    let Lowercase = useLowercase;
-    let Uppercase = useUppercase;
-    let Numbers = useNumbers;
-    let Symbols = useSymbols;
-    resultBtn.innerText = passwordGenerate(Lowercase, Uppercase, Numbers, Symbols, Length);
-  });
-  
+  let Length = totalLength;
+  let Lowercase = useLowercase;
+  let Uppercase = useUppercase;
+  let Numbers = useNumbers;
+  let Symbols = useSymbols;
+  resultBtn.innerText = passwordGenerate(Lowercase, Uppercase, Numbers, Symbols, Length);
+});
