cad84db728d7461306dd515c3a5288d9c38f17e9
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-05 21:09:12 -0500

    variables

---
+++ script.js
@@ -0,0 +1,8 @@
+let totalLength = window.prompt("Password Length:");
+let useNumbers = window.confirm("Use numbers?");
+let useUppercase = window.confirm("Use uppercase letters?");
+let useLowercase = window.confirm("Use lowercase letters?");
+let useSymbols = window.confirm("Use symbols?");
+let generateBtn = document.getElementById("generate");
+let resultBtn = document.getElementById("result");
+let symbols = '~`!@#$%^&*()_-+={[}]:;"<,>.?/|';
